25302cd099bcfd916ca1bd7dbf87515dd7ea4411
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-03 07:13:50 +0100

    added a second js solution

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// ticket pricing 
 let age= prompt("what is your age?");
   if (age <= 12) {
     alert("The price of your ticket is $10");
@@ -11,8 +11,36 @@ let age= prompt("what is your age?");
 else {
     alert("Sorry you are not eligible for this offer");
 }
-
-   
 // Children (age <= 12): $10
 //     Teenagers (age 13-17): $15
 //     Adults (age >= 18): $20
+
+// weather clothing advicer
+let weatherCondition = prompt("What is the condition of your weather?")
+switch (weatherCondition) {
+    case "sunny":
+        alert("Wear a t-shirt");
+        break;
+
+        case "cloudy":
+        alert("Wear a jacket");
+        break;
+
+        case "rainy":
+        alert("Wear a rain jacket");
+        break;
+
+        case "snowy":
+        alert("Wear a snow coat");
+        break;
+
+        case "humid":
+        alert("Wear a hat and a coat");
+        break;
+
+    default:
+        alert("weather condition is not defined");
+        break;
+}
+
+
