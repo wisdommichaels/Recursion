48c93a4280e3205fb80bbd0f6693f18bea4ffe1e
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-30 12:31:18 +0100

    version 1.4

--- listenerCount.js
+++ listenerCount.js
@@ -1,10 +1,49 @@
-//import event module
+// //import event module
+// var events = require('events');
+// // Create an eventEmitter object
+// var eventEmitter = new events.EventEmitter();
+// eventEmitter.on('connection',function(){
+//     console.log("hello connection")
+// });
+// // count the number of listener to this event
+// eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+// console.log(eventListeners + " Listner(s) listening to connection event");
+
+
 var events = require('events');
-// Create an eventEmitter object
 var eventEmitter = new events.EventEmitter();
-eventEmitter.on('connection',function(){
-    console.log("hello connection")
-});
-// count the number of listener to this event
+
+// listener #1
+var listner1 = function listner1() {
+   console.log('listner1 executed.');
+}
+
+// listener #2
+var listner2 = function listner2() {
+   console.log('listner2 executed.');
+}
+
+// Bind the connection event with the listner1 function
+eventEmitter.addListener('connection', listner1);
+
+// Bind the connection event with the listner2 function
+eventEmitter.on('connection', listner2);
+
+var eventListeners = require('events').EventEmitter.listenerCount
+   (eventEmitter,'connection');
+console.log(eventListeners + " Listner(s) listening to connection event");
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+// Remove the binding of listner1 function
+eventEmitter.removeListener('connection', listner1);
+console.log("Listner1 will not listen now.");
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
 eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
 console.log(eventListeners + " Listner(s) listening to connection event");
+
+console.log("Program Ended.");
