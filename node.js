6e81bad09ab95ad11d1d519c84528938fac71c80
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-30 11:44:32 +0100

    version 1.2

--- node.js
+++ node.js
@@ -1,26 +1,38 @@
-// Import events module
-var events = require('events');
+// // Import events module
+// var events = require('events');
 
-// Create an eventEmitter object
-var eventEmitter = new events.EventEmitter();
+// // Create an eventEmitter object
+// var eventEmitter = new events.EventEmitter();
 
-// Create an event handler as follows
-var connectHandler = function connected() {
-   console.log('connection succesful.');
+// // Create an event handler as follows
+// var connectHandler = function connected() {
+//    console.log('connection succesful.');
   
-   // Fire the data_received event 
-   eventEmitter.emit('data_received');
-}
+//    // Fire the data_received event 
+//    eventEmitter.emit('data_received');
+// }
 
-// Bind the connection event with the handler
-eventEmitter.on('connection', connectHandler);
+// // Bind the connection event with the handler
+// eventEmitter.on('connection', connectHandler);
  
-// Bind the data_received event with the anonymous function
-eventEmitter.on('data_received', function() {
-   console.log('data received succesfully.');
+// // Bind the data_received event with the anonymous function
+// eventEmitter.on('data_received', function() {
+//    console.log('data received succesfully.');
+// });
+
+// // Fire the connection event 
+// eventEmitter.emit('connection');
+
+// console.log("Program Ended.");
+
+
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+   if (err) {
+      console.log(err.stack);
+      return;
+   }
+   console.log(data.toString());
 });
-
-// Fire the connection event 
-eventEmitter.emit('connection');
-
-console.log("Program Ended.");
+console.log("Program Ended");
